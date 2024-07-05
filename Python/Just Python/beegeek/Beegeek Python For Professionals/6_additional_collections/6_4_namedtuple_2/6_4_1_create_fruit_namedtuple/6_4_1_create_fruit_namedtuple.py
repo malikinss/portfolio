3bf9feb8f5b498ccf8a449a48fd844cf6329ee9e
Sam Malikin <workmalikin@gmail.com>
2024-07-05 14:29:38 +0300

    Add new 6_4_1_create_fruit_namedtuple.py with initial implementation
    Add named tuple Fruit definition
    
    Implemented the Fruit named tuple with fields: name, color, vitamins
    Used the collections.namedtuple function to create the Fruit type
    Fields are specified as a space-separated string: 'name color vitamins'
    No additional code or output added, as per the requirements
    This commit introduces a concise definition of a Fruit named tuple, demonstrating the use of collections.namedtuple for creating simple, lightweight data structures in Python.

---
+++ Python/Just Python/beegeek/Beegeek Python For Professionals/6_additional_collections/6_4_namedtuple_2/6_4_1_create_fruit_namedtuple/6_4_1_create_fruit_namedtuple.py
@@ -0,0 +1,12 @@
+'''
+TODO:
+        Complete the code below so that it creates a named tuple Fruit with
+        fields:
+            name, color, vitamins
+
+NOTE:
+        The program should not output anything.
+'''
+from collections import namedtuple
+
+Fruit = namedtuple('Fruit', 'name color vitamins')
